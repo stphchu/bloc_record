cba11212e004802e410b71d77ab5f643b16aab8a
Stephanie <esceeaitchyu@gmail.com>
2020-02-07 17:05:09 -0800

    Checkpoint 04 completion

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -134,6 +134,68 @@ module Selection
     rows_to_array(rows)
   end
 
+# where
+  def where(*args)
+    if args.count > 1
+      expression = args.shift
+      params = args
+    else
+      case args.first
+      when String
+        expression = args.first
+      when Hash
+        expression_hash = BlocRecord::Utility.convert_keys(args.first)
+        expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+      end
+    end
+
+    sql = <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE #{expression};
+      SQL
+
+    rows = connection.execute(sql, params)
+    rows_to_array(rows)
+  end
+
+# order
+  def order(*args)
+    if args.count > 1
+      order = args.join(",")
+    else
+      order = args.first.to_s
+    end
+
+    rows = connection.execute <<-SQL
+      SELECT * FROM #{table}
+      ORDER BY #{order};
+      SQL
+    rows_to_array(rows)
+  end
+
+# join
+  def join(*args)
+    if args.count > 1
+      joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+      rows = connection.execute <<-SQL
+        SELECT * FROM #{table} #{joins}
+        SQL
+    else
+      case args.first
+      when String
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+          SQL
+      when Symbol
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table}
+          INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+          SQL
+      end
+    end
+    rows_to_array(rows)
+  end
+
 # PRIVATE
   private
 
